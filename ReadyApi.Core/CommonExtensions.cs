e0ff9d318313a398c8dd9e7206b10eea50a3fa61
Adem Catamak <ademcatamak@gmail.com>
2018-10-19 00:04:23 +0300

    general nuget package downgrade operation 2.1.1 to 2.0.4

---
+++ ReadyApi.Core/CommonExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace ReadyApi.Core
+{
+    public static class CommonExtensions
+    {
+        public static void Log(this ILogger logger, LogLevel logLevel, string message)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    logger.LogTrace(message);
+                    break;
+                case LogLevel.Debug:
+                    logger.LogDebug(message);
+                    break;
+                case LogLevel.Information:
+                    logger.LogInformation(message);
+                    break;
+                case LogLevel.Warning:
+                    logger.LogWarning(message);
+                    break;
+                case LogLevel.Error:
+                    logger.LogError(message);
+                    break;
+                case LogLevel.Critical:
+                    logger.LogCritical(message);
+                    break;
+                default:
+                    logger.LogError(message);
+                    break;
+            }
+        }
+    }
+}

33a03ca37256e98adc4abb2e4098aaed7113030e
Adem Catamak <adem.catamak@relateddigital.com>
2018-12-11 16:56:36 +0300

    process-time-watcher-middleware accept method. That method use for extract trace-id

--- ReadyApi.AspNetCore.Middleware/ProcessTimeWatcherMiddleware.cs
+++ ReadyApi.AspNetCore.Middleware/ProcessTimeWatcherMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -12,15 +13,17 @@ namespace ReadyApi.AspNetCore.Middleware
         private readonly ProcessTimeWatcherOptions _options;
         private readonly RequestDelegate _next;
         private readonly ILogger<ProcessTimeWatcherMiddleware> _logger;
+        private readonly Func<HttpContext, string> _getCorrelationId;
 
         public ProcessTimeWatcherMiddleware(RequestDelegate next, ILogger<ProcessTimeWatcherMiddleware> logger, IOptions<ProcessTimeWatcherOptions> options)
         {
             _options = options.Value;
             _next = next;
             _logger = logger;
+            _getCorrelationId = _options.GetCorrelationId ?? GetCorrelationId;
         }
 
-        public string GetCorrelationId(HttpContext httpContext)
+        private string GetCorrelationId(HttpContext httpContext)
         {
             return httpContext.TraceIdentifier;
         }
@@ -28,7 +31,9 @@ namespace ReadyApi.AspNetCore.Middleware
         public async Task Invoke(HttpContext httpContext)
         {
             string endpointName = httpContext.Request.Path;
-            string correlationId = GetCorrelationId(httpContext);
+            string verb = httpContext.Request.Method;
+
+            string correlationId = _getCorrelationId(httpContext);
 
             var watch = new Stopwatch();
             try
@@ -39,7 +44,7 @@ namespace ReadyApi.AspNetCore.Middleware
             finally
             {
                 watch.Stop();
-                string message = $"[{nameof(ProcessTimeWatcherMiddleware)}] : [{endpointName}] - [{correlationId}] - {watch.ElapsedMilliseconds} Ms";
+                string message = $"[{nameof(ProcessTimeWatcherMiddleware)}] : [{verb}] - [{endpointName}] - [{correlationId}] - {watch.ElapsedMilliseconds} Ms";
                 _logger.Log(_options.LogLevel, new EventId((int) _options.LogLevel), typeof(ProcessTimeWatcherMiddleware), null, (type, exception) => message);
             }
         }
@@ -48,7 +53,8 @@ namespace ReadyApi.AspNetCore.Middleware
     public class ProcessTimeWatcherOptions
     {
         public LogLevel LogLevel { get; set; } = LogLevel.Information;
-        public string CorrelationIdHeaderName { get; set; } = CorrelationIdMiddlewareOptions.DEFAULT_HEADER;
+        public Func<HttpContext, string> GetCorrelationId;
+
     }
 
     public static class ProcessTimeWatcherMiddlewareExtensions
